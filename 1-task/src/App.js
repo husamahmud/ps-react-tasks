9efb221cd060a3b26616f84645a3a7d858fd4fbf
Sameh ElGazar <selgazar95@gmail.com>
2024-08-01 16:03:03 +0300

    feat: implement a new feature

--- 1-task/src/App.js
+++ 1-task/src/App.js
@@ -1,13 +1,13 @@
 export default function App() {
   return (
-    <>
+    <div className="container">
       <header>Header</header>
-      <div>
+      <div className="content-container">
         <nav>Navigation</nav>
         <main>Main</main>
         <aside>Sidebar</aside>
       </div>
       <footer>Footer</footer>
-    </>
-  )
+    </div>
+  );
 }
