f07bb3d25d16dfb481ad34855bfc7259909f4c9c
husamahmud <husamahmud@gmail.com>
2024-08-01 03:33:55 +0300

    feat: configure `commitlint`

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'refactor', 'docs', 'style', 'test', 'chore'],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+  },
+}
