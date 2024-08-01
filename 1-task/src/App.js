483fa1f87bfce2c23511d07349bab71213e0bdb6
Gehadazzam <gehadgamalazzam@gmail.com>
2024-08-01 07:36:20 +0300

    feat: implement responsive layout with header, columns, and footer

--- 1-task/src/App.js
+++ 1-task/src/App.js
@@ -2,12 +2,12 @@ export default function App() {
   return (
     <>
       <header>Header</header>
-      <div>
+      <div className="main">
         <nav>Navigation</nav>
         <main>Main</main>
         <aside>Sidebar</aside>
       </div>
       <footer>Footer</footer>
     </>
-  )
+  );
 }
