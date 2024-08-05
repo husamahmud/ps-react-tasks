ee2190fb252568f80669cc581155c9595bcb8844
Ahmed Nassef <90284492+Ahmed-Nassef@users.noreply.github.com>
2024-08-05 08:12:29 +0300

    feat: finish the mortgage calculator

--- 2-task/husam/src/App.tsx
+++ 2-task/husam/src/App.tsx
@@ -1,24 +1,149 @@
+import { useEffect, useState } from "react";
+import InputContainer from "./components/bits/Input";
+import Label from "./components/bits/Label";
 import styles from "./styles.module.css";
 
 function App() {
+  const [userLoanInput, setUserLoanInput] = useState({
+    loanAmount: 0,
+    LoanTerm: 0,
+    Interest: 0,
+  });
+  const [calculationResult, setCalculationResult] = useState("");
+
+  useEffect(() => {
+    calculateLoan();
+  }, [userLoanInput]);
+
+  const calculateLoan = () => {
+    const { Interest, LoanTerm: term, loanAmount: amount } = userLoanInput;
+
+    if (Interest <= 0 || term <= 0 || amount <= 0) {
+      return;
+    }
+
+    const monthlyInterestRate = Interest / 12 / 100;
+
+    // Convert term from years to months
+    const termInMonths = term * 12;
+
+    // Calculate the monthly payment
+    const monthlyPayment =
+      (amount *
+        monthlyInterestRate *
+        Math.pow(1 + monthlyInterestRate, termInMonths)) /
+      (Math.pow(1 + monthlyInterestRate, termInMonths) - 1);
+
+    const formattedMonthlyPayment = new Intl.NumberFormat("en-US", {
+      currency: "USD",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(monthlyPayment);
+
+    setCalculationResult(formattedMonthlyPayment);
+  };
+
   return (
     <section id="body" className={styles.body}>
       <div className={styles.calculator__container}>
         <h1>Calulator</h1>
         <div className={styles.calculator__body}>
-          <div className="left" style={{ flex: 1 }}>
-            <div className="tabs__links">
-              <button className={styles.tab_link} role="tablist">
-                monthly
+          <div className="left flex-1">
+            <div className={styles.tabs__links}>
+              <button
+                className={styles.tab_link}
+                // active state
+                role="tablist"
+              >
+                Monthly Payment
               </button>
+              <div
+                className="separator"
+                style={{
+                  padding: "0.75rem 0",
+                  width: "1px",
+                  backgroundColor: "gray",
+                  display: "block",
+                }}
+              ></div>
               <button className={styles.tab_link} role="tablist">
-                payment
+                Purshace Budget
               </button>
             </div>
-            <div className="tab__body">BODY</div>
+            {/* TODO: implement tab feature */}
+            <div className="tab__body text-sm">
+              <div className="loan_amount mt-4">
+                <InputContainer
+                  value={userLoanInput.loanAmount}
+                  onChange={(e) => {
+                    setUserLoanInput((prev) => {
+                      return { ...prev, loanAmount: Number(e.target.value) };
+                    });
+                  }}
+                  placeholder="Loan Amount"
+                  className="w-full"
+                  type="number"
+                  id="loan_amount"
+                >
+                  <Label>Loan Amount</Label>
+                </InputContainer>
+              </div>
+              <div className="term_interest flex gap-6  items-center mt-4">
+                {/* WHAT THE FUCK IS THIS BEHAVIOR ???????????????????? */}
+                <InputContainer
+                  value={userLoanInput.LoanTerm}
+                  type="number"
+                  id="loan_term"
+                  containerClassName="flex-1"
+                  className="w-full"
+                  placeholder="Loan Term"
+                  prefix=""
+                  onChange={(e) => {
+                    setUserLoanInput((prev) => {
+                      return { ...prev, LoanTerm: Number(e.target.value) };
+                    });
+                  }}
+                >
+                  <Label>Loan Term</Label>
+                </InputContainer>
+                <InputContainer
+                  value={userLoanInput.Interest}
+                  className="max-w-28"
+                  type="number"
+                  id="loan_interest"
+                  prefix="%"
+                  onChange={(e) => {
+                    setUserLoanInput((prev) => {
+                      return { ...prev, Interest: Number(e.target.value) };
+                    });
+                  }}
+                >
+                  <Label>Interest</Label>
+                </InputContainer>
+              </div>
+            </div>
           </div>
-          <div className="right" style={{ flex: 1 }}>
-            result
+          <div
+            className="separator"
+            style={{
+              padding: "0.75rem 0",
+              width: "1px",
+              backgroundColor: "gray",
+              display: "block",
+              margin: "0 1rem",
+            }}
+          ></div>
+          <div className="right flex-1">
+            {calculationResult ? (
+              <>
+                <h3 className="text-sm">Monthly Payment</h3>
+                <p>
+                  $ <span className="text-3xl">{calculationResult}</span>
+                </p>
+              </>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </div>
---
+++ 2-task/husam/src/components/bits/Label.tsx
@@ -0,0 +1,17 @@
+import React, { DetailedHTMLProps, LabelHTMLAttributes } from "react";
+
+interface LabelProps
+  extends DetailedHTMLProps<
+    LabelHTMLAttributes<HTMLLabelElement>,
+    HTMLLabelElement
+  > {}
+
+const Label = ({ htmlFor, children, ...props }: LabelProps) => {
+  return (
+    <label htmlFor={htmlFor} {...props}>
+      {children}
+    </label>
+  );
+};
+
+export default Label;
