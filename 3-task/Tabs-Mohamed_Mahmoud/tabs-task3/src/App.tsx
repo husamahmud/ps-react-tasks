ea144ff9324e77525dfc8019fb070fa47da60743
Mohamed-Leo <mohamedlewandoski2018@gmail.com>
2024-08-11 23:07:37 +0300

    feat : implement a new feature

---
+++ 3-task/Tabs-Mohamed_Mahmoud/tabs-task3/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+import Tabs from './components/Tabs'
+
+function App() {
+
+  return (
+    <>
+      <Tabs />
+    </>
+  )
+}
+
+export default App;
---
+++ 3-task/Tabs-Mohamed_Mahmoud/tabs-task3/src/components/Tabs.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function Tabs() {
+    return (
+        <div>
+            Tabs
+        </div>
+    )
+}
+
+export default Tabs;
